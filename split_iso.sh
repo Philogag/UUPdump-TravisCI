628f18b0c5942e8a7959a147e65343644af68759
philogag <philogag@qq.com>
2021-09-04 19:05:38 +0800

    .

--- split_iso.sh
+++ split_iso.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo "hello" > test.iso
-
-ls .
 mkdir ./isobuild
 find . -type f -name "*.iso" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
+echo ""
+echo "Built:"
 ls ./isobuild
