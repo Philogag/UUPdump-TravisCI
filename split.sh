23bf7429f4fcb986160cb8a9f38d609a695d4661
philogag <philogag@qq.com>
2021-09-04 21:46:37 +0800

    ISO insteadof iso

--- split.sh
+++ split.sh
@@ -2,7 +2,7 @@
 
 # Split ISO files
 mkdir ./isobuild
-find . -type f -name "*.iso" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
+find . -type f -name "*.ISO" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
 echo ""
 echo "Built:"
 ls ./isobuild
