5a2034fa60bb26540be00b5f57b46474c9b06903
philogag <philogag@qq.com>
2021-09-04 11:56:39 +0800

    .

--- uup_download_linux.sh
+++ uup_download_linux.sh
@@ -78,7 +78,3 @@ if [ -e ./files/convert.sh ]; then
   chmod +x ./files/convert.sh
   ./files/convert.sh wim "$destDir" 0
 fi
-
-mkdir ./isobuild
-find . -type f -name "*.iso" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
-ls ./isobuild
