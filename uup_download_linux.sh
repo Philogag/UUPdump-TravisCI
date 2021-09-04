3f0d2d93172f03d2201939f648279b1564ef4553
philogag <philogag@qq.com>
2021-09-04 11:33:28 +0800

    add zip split for release

--- uup_download_linux.sh
+++ uup_download_linux.sh
@@ -80,5 +80,5 @@ if [ -e ./files/convert.sh ]; then
 fi
 
 mkdir ./isobuild
-find . -type f -name "*.iso" ! -path "./isobuild/*" -exec mv {} ./isobuild
+find . -type f -name "*.iso" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
 ls ./isobuild
