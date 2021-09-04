5a91d4ddb4350ffcb81561f0873739770affd8a6
philogag <philogag@qq.com>
2021-09-04 11:02:27 +0800

    detect built iso-file

--- uup_download_linux.sh
+++ uup_download_linux.sh
@@ -77,6 +77,6 @@ echo ""
 if [ -e ./files/convert.sh ]; then
   chmod +x ./files/convert.sh
   ./files/convert.sh wim "$destDir" 0
-  mkdir ./isobuild
-  mv ./*.iso ./isobuild
+  find . -type f -name "*.iso" ! -path "./isobuild/*" -exec mv {} "./isobuild"
+  ls ./isobuild
 fi
