33354075ac0aacd447488cde5435a4230ae571f4
philogag <philogag@qq.com>
2021-09-04 19:49:11 +0800

    .

---
+++ before_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Split ISO files
+mkdir ./isobuild
+find . -type f -name "*.iso" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
+echo ""
+echo "Built:"
+ls ./isobuild
+
+# Set up git user name and tag this commit
+git config --local user.name "philogag"
+git config --local user.email "philogag@qq.com"
+export TRAVIS_TAG=${TRAVIS_TAG:-$(date +%Y%m%d)}
+git tag $TRAVIS_TAG
+
+# Create Release Body
+python3 ./create_release_body.py
+export $RELEASE_BODY=$(cat body.md)
--- split_iso.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir ./isobuild
-find . -type f -name "*.iso" ! -path "./isobuild/*" -exec zip -r -0 -s 1g ./isobuild/{}.zip {} \;
-echo ""
-echo "Built:"
-ls ./isobuild
